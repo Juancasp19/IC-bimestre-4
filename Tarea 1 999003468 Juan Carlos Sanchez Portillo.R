84e044209f610604985b718077067d836c3ee7ae
Juancasp19 <70883660+Juancasp19@users.noreply.github.com>
2020-09-07 02:39:11 -0600

    Add files via upload

---
+++ Tarea 1 999003468 Juan Carlos Sanchez Portillo.R
@@ -0,0 +1,19 @@
+> library(readr)
+> tabla <- read_csv("C:/Users/Juan Carlos/Downloads/Car.csv")
+
+
+library(psych)
+
+library(modeest)
+
+
+mean(tabla$speed1)
+
+
+mfv(tabla$type4)
+
+
+sd(tabla$pollution3)
+sd(tabla$pollution5)
+
+
